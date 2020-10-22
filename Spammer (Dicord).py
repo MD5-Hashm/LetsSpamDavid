c7125fdfa910e756e31919e7165255b711596704
MD5-Hashm <47696465+MD5-Hashm@users.noreply.github.com>
2020-10-22 13:45:14 -0400

    Add files via upload

---
+++ Spammer (Dicord).py
@@ -0,0 +1,45 @@
+import random
+import time
+from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Listener
+
+keyboard = Controller()
+
+hi = 1
+
+time.sleep(4)
+
+while hi == 1:
+    keyboard.type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.523)
+    keyboard.type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.523)
+    keyboard.type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.523)
+    keyboard.type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.523)
+    keyboard.type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.523)
+    keyboard.type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.523)
+    keyboard.type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.523)
+    keyboard.type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.4)
+    time.sleep(3)
---
+++ Spammer (No antispam).py
@@ -0,0 +1,16 @@
+import random
+import time
+from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Listener
+
+keyboard = Controller()
+
+hi = 1
+
+time.sleep(4)
+
+while hi == 1:
+    keyboard.type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.23)
